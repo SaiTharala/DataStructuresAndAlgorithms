223396c81cc05035c5aa85b840444a4b976312ff
unknown <Saitharala@gmail.com>
2021-11-29 21:27:05 +0530

    [ Tharala SaiPrashanth] Added  UnOrderedList Feauture

--- DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/Program.cs
@@ -6,9 +6,24 @@ namespace DataStructuresAndAlgorithm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            BankingCashCounter bank = new BankingCashCounter();
-            bank.Counter();
+            Console.WriteLine("Welcome to Data Structures And Alogorithm Programs...");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nChoose an Option : \n 1.Unordered List\n 2.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UnOrderedList unordered = new UnOrderedList();
+                        unordered.Read();
+                        unordered.Search("R");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/UnOrderedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataStructuresAndAlgorithm
+{
+    class UnOrderedList
+    {
+        public static LinkedList<string> List = new LinkedList<string>();
+        public static string filepath = @"E:\GIT BL\DataStructuresAndAlgorithms\DataStructuresAndAlgorithms\DataStructuresAndAlgorithm\DataStructuresAndAlgorithm\Utility\UnOrderedclass.txt";
+        public static string data = File.ReadAllText(filepath);
+        string[] words = data.Split(",");
+        public void Read()
+        {
+            foreach (var result in words)
+            {
+                List.Add(result);
+            }
+        }
+        public void Search(string value)
+        {
+            int i = 0;
+            while (i < words.Length)
+            {
+                if (value == words[i])
+                {
+                    Console.WriteLine("Word Found");
+                    List.Delete(words[i]);
+                    this.Write();
+                    return;
+                }
+                i++;
+            }
+            Console.WriteLine("not found\n");
+            List.Add(value);
+            this.Write();
+        }
+        public void Write()
+        {
+            string text = List.Updated();
+            File.WriteAllText(filepath, text);
+        }
+    }
+
+}
