9bf0a6ba22d05e316182a48da400828f87bd2cdf
unknown <Saitharala@gmail.com>
2021-11-29 21:39:26 +0530

    [ Tharala SaiPrashanth] Added  OrderedList Feauture

--- DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/DataStructuresAndAlgorithm/Program.cs
@@ -2,28 +2,33 @@
 
 namespace DataStructuresAndAlgorithm
 {
-    class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
-            Console.WriteLine("Welcome to Data Structures And Alogorithm Programs...");
-            bool flag = true;
-            while (flag)
+            static void Main(string[] args)
             {
-                Console.WriteLine("\nChoose an Option : \n 1.Unordered List\n 2.Exit\n");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
+                Console.WriteLine("Welcome to Data Structures And Alogorithm Programs...");
+                bool flag = true;
+                while (flag)
                 {
-                    case 1:
-                        UnOrderedList unordered = new UnOrderedList();
-                        unordered.Read();
-                        unordered.Search("R");
-                        break;
-                    case 2:
-                        flag = false;
-                        break;
+                    Console.WriteLine("\nChoose an Option : \n 1.Unordered List\n 2.OrderedList\n 3.Exit\n");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            UnOrderedList unordered = new UnOrderedList();
+                            unordered.Read();
+                            unordered.Search("R");
+                            break;
+                        case 2:
+                            OrderedList ordered = new OrderedList();
+                            ordered.Read();
+                            ordered.Search(2);
+                            break;
+                        case 3:
+                            flag = false;
+                            break;
+                    }
                 }
             }
         }
     }
-}
